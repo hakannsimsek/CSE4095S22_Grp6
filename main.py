8293805b5938f31c1fd65cab59d3223a3df24030
Bera <bera.koc@getir.com>
2022-04-05 12:14:54 +0300

    Added file reader for various scenarios

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 import re
+from numpy import number
 import scipy.stats
+from os import listdir
+from os.path import isfile, join
 # from snowballstemmer import TurkishStemmer
 
 # turk_stemmer = TurkishStemmer()
@@ -12,6 +15,9 @@ import scipy.stats
 
 
 # Chai Square Test
+critical_chai_square_value = scipy.stats.chi2.ppf(1-.05, df=1)
+print(critical_chai_square_value)
+
 def calculate_chai_square(word_frequency_matrix, number_of_tokens_in_corpus):
     return number_of_tokens_in_corpus * pow((word_frequency_matrix[0][0] * word_frequency_matrix[1][1]) - (word_frequency_matrix[0][1] * word_frequency_matrix[1][0]), 2) / ((word_frequency_matrix[0][0] + word_frequency_matrix[1][0]) * (word_frequency_matrix[0][1] + word_frequency_matrix[1][1]) * (word_frequency_matrix[0][0] + word_frequency_matrix[0][1]) * (word_frequency_matrix[1][0] + word_frequency_matrix[1][1]))
 
@@ -37,13 +43,22 @@ def get_top_ten_for_chai_square(word_map, number_of_tokens_in_corpus):
     chai_square_list.sort(key=lambda x: x[2], reverse=True)
     return chai_square_list[:10]
 
-critical_chai_square_value = scipy.stats.chi2.ppf(1-.05, df=1)
-
 def clear_punctuations(payload):
     return re.sub(r'[^\w\s]','',payload)
 
-def read_all_data_and_get_payloads():
-    return
+def read_all_data_and_get_payloads(path='data'):
+    jsonFileNames = [f for f in listdir(path) if isfile(join(path, f))]
+    payloads = []
+    for jsonFileName in jsonFileNames:
+        payloads.append(read_json_file_and_get_payload(join(path, jsonFileName)))
+    return payloads
+
+def read_some_data_and_get_payloads(path='data', number_of_files=10):
+    jsonFileNames = [f for f in listdir(path) if isfile(join(path, f))]
+    payloads = []
+    for jsonFileName in jsonFileNames[:number_of_files]:
+        payloads.append(read_json_file_and_get_payload(join(path, jsonFileName)))
+    return payloads
 
 def read_json_file_and_get_payload(file_name):
     with open(file_name, 'r') as f:
@@ -51,11 +66,13 @@ def read_json_file_and_get_payload(file_name):
     return clear_punctuations(data['ictihat'].strip()).split(' ')
  
 # payload = list(map(turk_stemmer.stemWord, clear_punctuations(data['ictihat'].strip()).split(' '))) # We should remove spaces in the text
-payload = read_json_file_and_get_payload('./1.json')
-number_of_tokens_in_corpus = len(payload)
+# payload = read_json_file_and_get_payload('./1.json')
+# number_of_tokens_in_corpus = len(payload)
+payloads = read_some_data_and_get_payloads()
 
-def getWordMap():
-    word_map = {}
+word_map = {}
+
+def getWordMap(payload):
     gram_count = 5
     indicator = gram_count // 2
 
@@ -78,12 +95,16 @@ def getWordMap():
                 frequency_map[-indicator + j][gram] += 1
             else:
                 frequency_map[-indicator + j][gram] = 1
-    return word_map
+    return len(payload)
 
-word_map = getWordMap()
+number_of_tokens_in_corpus_sum = 0
 
-print(construct_frequency_matrix_from_word_map('yaralama', 'suçundan', word_map, number_of_tokens_in_corpus))
-print(get_top_ten_for_chai_square(word_map, number_of_tokens_in_corpus))
+for payload in payloads:
+    number_of_tokens_in_corpus_sum += getWordMap(payload)
+print(number_of_tokens_in_corpus_sum)
+
+# print(construct_frequency_matrix_from_word_map('yaralama', 'suçundan', word_map, number_of_tokens_in_corpus))
+print(get_top_ten_for_chai_square(word_map, number_of_tokens_in_corpus_sum))
 
 # for i in range(-2, 2):
 #     for gram in ve[i]:
