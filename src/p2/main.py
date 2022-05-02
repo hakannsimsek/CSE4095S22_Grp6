41d64368971f23d5cc5fc0039981ff4f73493ff3
Bera <bera.koc@getir.com>
2022-05-02 22:07:55 +0300

    Implemented SVM with proper setup

--- src/p2/main.py
+++ src/p2/main.py
@@ -1,7 +1,10 @@
 from ntpath import join
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import pandas as pd
-from reader import Extractor
+from sklearn.model_selection import train_test_split
+from reader import Reader
+from sklearn import datasets, svm
+
 corpus = [
     'This is the first document.',
     'This document is the second document.',
@@ -9,42 +12,77 @@ corpus = [
     'Is this the first document?',
 ]
 
-countvectorizer = CountVectorizer(analyzer= 'word')
-tfidfvectorizer = TfidfVectorizer(analyzer='word')
-# convert th documents into a matrix
-count_wm = countvectorizer.fit_transform(corpus)
-tfidf_wm = tfidfvectorizer.fit_transform(corpus)
-#retrieve the terms found in the corpora
-# if we take same parameters on both Classes(CountVectorizer and TfidfVectorizer) , it will give same output of get_feature_names() methods)
-#count_tokens = tfidfvectorizer.get_feature_names() # no difference
-count_tokens = countvectorizer.get_feature_names()
-tfidf_tokens = tfidfvectorizer.get_feature_names()
-df_countvect = pd.DataFrame(data = count_wm.toarray(),index = ['Doc1','Doc2','Doc3', 'Doc4'],columns = count_tokens)
-df_tfidfvect = pd.DataFrame(data = tfidf_wm.toarray(),index = ['Doc1','Doc2','Doc3', 'Doc4'],columns = tfidf_tokens)
-print("Count Vectorizer\n")
-print(df_countvect)
-print("\nTD-IDF Vectorizer\n")
-print(df_tfidfvect)
+def get_count_vm_and_features(docs):
+    vectorizer = CountVectorizer()
+    count_vm = vectorizer.fit_transform(docs)
+    return count_vm, vectorizer.get_feature_names_out()
 
-crimeMap = Extractor.read_all_data_and_get_crime_and_corpus()
-sort_orders = sorted(crimeMap.items(), key=lambda x: x[1], reverse=True)
-print(sort_orders)
+def get_tfidf_vm_and_features(docs, max_features=5000):
+    vectorizer = TfidfVectorizer(max_features=max_features)
+    tfidf_vectorizer = vectorizer.fit_transform(docs)
+    return tfidf_vectorizer, vectorizer.get_feature_names_out()
 
-# crimeCorpusMap = {}
+def print_vectorizer_result(tag, vm, features):
+    print('{} Vectorizer\n'.format(tag))
+    print(pd.DataFrame(data=vm.toarray(), columns=features, index=range(vm.shape[0])))
 
-# for i in range(1,1):
-#     jsonFileName = 'data/' + str(i) + '.json'
-#     content = Extractor.read_json_file(jsonFileName)
-#     crimeName = content['Suç']
-#     if crimeName == '':
-#         if 'undefined' not in crimeCorpusMap:
-#             crimeCorpusMap['undefined'] = 0
-#         crimeName = 'undefined'
-#     if content['Suç'] not in crimeCorpusMap:
-#         crimeCorpusMap[content['Suç']] = 0
-#     print(crimeName)
-#     # crimeCorpusMap[content['Suç']] = crimeCorpusMap[content['Suç']].append(content['ictihat'])
-#     crimeCorpusMap[crimeName] = crimeCorpusMap[crimeName] + 1
+def get_top_n_crime_names(crime_map, n=10, reverse=True):
+    sorted_crime_list = sorted(crime_map.items(), key=lambda x: x[1]['count'], reverse=reverse)
+    first_n_crime_list = sorted_crime_list[:n]
+    first_n_crime_names = []
+    for crime in first_n_crime_list:
+        first_n_crime_names.append(crime[0])
+    return first_n_crime_names
+
+def construct_y_by_doc_crime_list(doc_crime_list):
+    y = doc_crime_list
+    return y
+
+def print_crime_map_by(crime_map, by='count'):
+    for crime in crime_map:
+        print('{} - {}'.format(crime, crime_map[crime][by]))
+
+def get_optimized_crime_map(crime_map, top_n_crime_names):
+    others_key = 'Others'
+    optimized_crime_map = { 'Others': { 'corpus': [], 'count': 0 } }
+    for index, crimeName in enumerate(crime_map):
+        if crimeName in top_n_crime_names:
+            optimized_crime_map[crimeName] = crime_map[crimeName]
+        else:
+            optimized_crime_map[others_key]['corpus'] += crime_map[crimeName]['corpus']
+            optimized_crime_map[others_key]['count'] += crime_map[crimeName]['count']
+    return optimized_crime_map
+
+def get_optimized_doc_crime_list(doc_crime_list, top_n_crime_names):
+    outlier_key = 'Others'
+    optimized_doc_crime_list = []
+    for crimeName in doc_crime_list:
+        if crimeName not in top_n_crime_names:
+            optimized_doc_crime_list.append(crimeName)
+        else:
+            optimized_doc_crime_list.append(outlier_key)
+    return optimized_doc_crime_list
 
 
-# print(crimeCorpusMap)
+# This area is common DO NOT change it
+print('Obtaining necessary data values...')
+crime_map, docs, doc_crime_list = Reader.read_all_data_and_get_crime_map_and_docs_and_doc_crime_list()
+top_n_crime_names = get_top_n_crime_names(crime_map)
+optimized_crime_map = get_optimized_crime_map(crime_map, top_n_crime_names)
+optimized_doc_crime_list = get_optimized_doc_crime_list(doc_crime_list, top_n_crime_names=top_n_crime_names)
+print_crime_map_by(optimized_crime_map, by='count')
+tfidf_vm, tfidf_features = get_tfidf_vm_and_features(docs)
+# print_vectorizer_result('TF-IDF', tfidf_vm, tfidf_features)
+X = tfidf_vm.toarray()
+y = construct_y_by_doc_crime_list(optimized_doc_crime_list)
+print('Splitting data for training and testing...')
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# End of the common area
+
+# SVM Part Start
+# Works so damn slow and that's why: https://stackoverflow.com/questions/40077432/why-is-scikit-learn-svm-svc-extremely-slow
+print('Initiating Support Vector Machine Algorithm...')
+clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+print(clf.score(X_test, y_test))
+# SVM Part End
+
--- src/p2/reader.py
+++ src/p2/reader.py
@@ -5,10 +5,10 @@ import re
 from snowballstemmer import TurkishStemmer
 turkStem = TurkishStemmer()
 
-class Extractor:
+class Reader:
     def __new__(cls):
         if not hasattr(cls, 'instance'):
-          cls.instance = super(Extractor, cls).__new__(cls)
+          cls.instance = super(Reader, cls).__new__(cls)
         return cls.instance
 
     @staticmethod
@@ -20,32 +20,37 @@ class Extractor:
         jsonFileNames = [f for f in listdir(path) if isfile(join(path, f))]
         payloads = []
         for jsonFileName in jsonFileNames:
-            payloads.append(Extractor.read_json_file_and_get_payload(join(path, jsonFileName)))
+            payloads.append(Reader.read_json_file_and_get_payload(join(path, jsonFileName)))
         return payloads
     
     @staticmethod
-    def read_all_data_and_get_crime_and_corpus(path='data'):
-        jsonFileNames = [ str(i) + '.json' for i in range(1, 27842) ]
+    def read_all_data_and_get_crime_map_and_docs_and_doc_crime_list(path='data'):
+        jsonFileNames = [ str(i) + '.json' for i in range(1, 1000) ]
         crimeCorpusMap = {}
+        doc_crime_list = []
+        docs = []
         for jsonFileName in jsonFileNames:
-            content = Extractor.read_json_file(join(path, jsonFileName))
+            content = Reader.read_json_file(join(path, jsonFileName))
             crimeName = content['Suç']
             if crimeName == '':
                 if 'undefined' not in crimeCorpusMap:
-                    crimeCorpusMap['undefined'] = 0
+                    crimeCorpusMap['undefined'] = { 'corpus': [], 'count': 0 }
                 crimeName = 'undefined'
-            if content['Suç'] not in crimeCorpusMap:
-                crimeCorpusMap[content['Suç']] = 0
-            # crimeCorpusMap[content['Suç']] = crimeCorpusMap[content['Suç']].append(content['ictihat'])
-            crimeCorpusMap[crimeName] = crimeCorpusMap[crimeName] + 1
-        return crimeCorpusMap
+            if crimeName not in crimeCorpusMap:
+                crimeCorpusMap[content['Suç']] = { 'corpus': [], 'count': 0 }
+            doc_crime_list.append(crimeName)
+            crimeCorpusMap[crimeName]['corpus'].append(content['ictihat'])
+            crimeCorpusMap[crimeName]
+            docs.append(content['ictihat'])
+            crimeCorpusMap[crimeName]['count'] = crimeCorpusMap[crimeName]['count'] + 1
+        return crimeCorpusMap, docs, doc_crime_list
     
     @staticmethod
     def read_some_data_and_get_payloads(path='data', number_of_files=100):
         jsonFileNames = [f for f in listdir(path) if isfile(join(path, f))]
         payloads = []
         for jsonFileName in jsonFileNames[:number_of_files]:
-            payloads.append(Extractor.read_json_file_and_get_payload(join(path, jsonFileName)))
+            payloads.append(Reader.read_json_file_and_get_payload(join(path, jsonFileName)))
         return payloads
 
     @staticmethod
@@ -53,9 +58,9 @@ class Extractor:
         jsonFileNames = [f for f in listdir(path) if isfile(join(path, f))]
         payloads = []
         for jsonFileName in jsonFileNames:
-            data = Extractor.read_json_file(join(path, jsonFileName))
+            data = Reader.read_json_file(join(path, jsonFileName))
             if data['Mahkeme Günü'] == day:
-                payloads.append(Extractor.get_payload(data))
+                payloads.append(Reader.get_payload(data))
         return payloads
 
     @staticmethod
@@ -63,9 +68,9 @@ class Extractor:
         jsonFileNames = [f for f in listdir(path) if isfile(join(path, f))]
         payloads_day_map = {}
         for jsonFileName in jsonFileNames:
-            data = Extractor.read_json_file(join(path, jsonFileName))
+            data = Reader.read_json_file(join(path, jsonFileName))
             day = data['Mahkeme Günü']
-            payloads_day_map[day] = payloads_day_map.get(day, []) + [Extractor.get_payload(data)]
+            payloads_day_map[day] = payloads_day_map.get(day, []) + [Reader.get_payload(data)]
         return payloads_day_map
 
     @staticmethod
@@ -76,12 +81,12 @@ class Extractor:
 
     @staticmethod
     def get_payload(data):
-        payload = Extractor.clear_punctuations(data['ictihat'].lower().strip()).split(' ')
+        payload = Reader.clear_punctuations(data['ictihat'].lower().strip()).split(' ')
         return [ele for ele in payload if ele.strip()]
 
     @staticmethod
     def read_json_file_and_get_payload(file_name):
-        return Extractor.get_payload(Extractor.read_json_file(file_name))
+        return Reader.get_payload(Reader.read_json_file(file_name))
 
     @staticmethod
     def getPlainTextFromPayloads(payloads):
